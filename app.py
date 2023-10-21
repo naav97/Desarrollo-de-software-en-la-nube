bf829c48ca47cbc64ba6e024af3b7faa703e9424
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2023-10-21 08:51:52 -0500

    Added list and delete tarea (#3)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from modelos import db
+from vistas import TareaResource, TareaBorrarResource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:toor@localhost/flask_db'
@@ -21,3 +22,8 @@ db.create_all()
 cors = CORS(app)
 
 api = Api(app)
+api.add_resource(TareaResource, '/tasks')
+api.add_resource(TareaBorrarResource, '/tasks/<int:tarea_id>')
+
+if __name__=="__main__":
+    app.run(port=5001)
