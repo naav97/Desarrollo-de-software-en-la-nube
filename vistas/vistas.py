2dfbb69b77429957aeeee00308c879dabb10635e
Nicolas Alvarado <na.alvarado@uniandes.edu.co>
2023-10-21 11:01:14 -0500

    ya crea la tarea y recive el archivo

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+import hashlib
 from modelos import db, Usuario, Tarea, TareaSchema, UsuarioSchema
 from flask_restful import Resource
 from flask import request
-from werkzeug.utils import secure_filename
 from celery import shared_task
 from flask_jwt_extended import create_access_token, jwt_required
-import hashlib
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 tareas_schema = TareaSchema(many=True)
 usario_schema = UsuarioSchema()
@@ -31,27 +32,7 @@ def process_file(old_filename, new_filename, taskId):
     except Exception as e:
         return str(e)
 
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'mp4', 'm4a', 'm4p', 'm4b', 'm4r', 'm4v', 'webm', 'avi', 'mpeg', 'wmv'}
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@shared_task(ignore_result=False)
-def process_file(old_filename, new_filename, taskId):
-    uploaded_file = os.path.join('./uploads', old_filename)
-    processed_file = os.path.join('./uploads', new_filename)
-    cmd = ['ffmpeg', '-i', uploaded_file,  processed_file]
-    try:
-        subprocess.run(cmd, check=True)
-        task = Tarea.query.filter(Tarea.id == taskId).first()
-        task.estado = "processed"
-        db.session.commit()
-        return True
-    except Exception as e:
-        return str(e)
-
-class TareasResource(Resource):
+class TareaResource(Resource):
     def get(self):
         tareas = Tarea.query.all()
         
