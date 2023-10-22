83923e6fd1ec62e04e91eff494412c19d1bb377f
dfcruza <60239198+dfcruza@users.noreply.github.com>
2023-10-21 20:16:41 -0500

    Vistas del log in / sign in terminadas

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from modelos import db
-from vistas import TareaResource, TareaBorrarResource
+from vistas import TareaResource, TareaBorrarResource, VistaSignIn, VistaLogIn
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:toor@localhost/flask_db'
@@ -24,6 +24,8 @@ cors = CORS(app)
 api = Api(app)
 api.add_resource(TareaResource, '/tasks')
 api.add_resource(TareaBorrarResource, '/tasks/<int:tarea_id>')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
 
 if __name__=="__main__":
     app.run(port=5001)
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,8 +1,12 @@
-from modelos import db, Usuario, Tarea, TareaSchema
+from modelos import db, Usuario, Tarea, TareaSchema, UsuarioSchema
 from flask_restful import Resource
 from flask import request
+from flask_jwt_extended import create_access_token
+import hashlib
+import json
 
 tareas_schema = TareaSchema(many=True)
+usario_schema = UsuarioSchema()
 
 class TareaResource(Resource):
     def get(self):
@@ -27,4 +31,38 @@ class TareaBorrarResource(Resource):
 
 class VistaSignIn():
     def post(self):
-        pass
+        usuario = Usuario.query.filter_by(usuario=request.json['usuario']).first()
+        if usuario is None:
+            contrasena_encriptada = hashlib.md5(request.json['contrasena'].encode('utf-8')).hexdigest()
+            nuevo_usuario = Usuario(usuario=request.json['usuario'], contrasena=contrasena_encriptada)
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            return {"message": "Usuario creado con éxito"}, 201
+        else:
+            return {"message": "Usuario ya existe"}, 409
+    
+    def put(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.contrasena= request.json.get('contrasena', usuario.contrasena)
+        db.session.commit()
+        return usario_schema.dump(usuario)
+    
+    def delete(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return '', 204
+
+class VistaLogIn():
+
+    def post(self):
+        contrasena_encriptada = hashlib.md5(request.json['contrasena'].encode('utf-8')).hexdigest()
+        usuario = Usuario.query.filter(Usuario.usuario == request.json['usuario'], Usuario.contrasena == contrasena_encriptada).first()
+
+        db.session.commit()
+
+        if usuario is None:
+            return "El usuario no existe", 404
+        else:
+            token_de_acceso = create_access_token(identity=usuario.id)
+            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": usuario.id, "username": usuario.usuario, "idParent": usuario.parent_id}, 200
