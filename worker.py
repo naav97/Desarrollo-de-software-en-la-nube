d0528f4274f5a3240bbc3bdabfaedd93adeac65d
Giancarlo <corredorcatino@users.noreply.github.com>
2023-11-19 23:44:38 -0500

    Worker con pub sub (#23)
    * ya envia archivo al bucket desde el worker (#21)
    
    * ya envia archivo
    
    * se quita linea
    
    * intento de leer subs
    
    * fix
    
    * ack
    
    * ya funciona
    
    * ultimos cambios
    
    ---------
    
    Co-authored-by: Nicolas Alvarado <na.alvarado@uniandes.edu.co>

--- worker.py
+++ worker.py
@@ -1,29 +1,37 @@
 import os
 import subprocess
-from celery import Celery
+import time
+import json
 from modelos import Tarea
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from google.cloud.sql.connector import Connector, IPTypes
+from google.cloud import pubsub_v1
 
+# initialize Python Connector object
+connector = Connector()
 
-app = Celery(broker= "redis://redis:6379/0",backend= "redis://redis:6379/0")
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:contraseña@db:5432/conversor'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['PROPAGATE_EXCEPTIONS'] = True
-# app.config['SQLALCHEMY_ECHO'] = True
+# Python Connector database connection function
+def getconn():
+    conn = connector.connect(
+        "project:region:instance", # Cloud SQL Instance Connection Name
+        "pg8000",
+        user="user",
+        password="password",
+        db="db_name",
+        ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+    )
+    return conn
 
-db_uri = 'postgresql://postgres:contraseña@db:5432/conversor'
-engine = create_engine(db_uri)
+engine = create_engine('postgresql+pg8000://', creator=getconn)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@app.task(name="process_file")
 def process_file(old_filename, new_filename, taskId):
-    uploaded_file = os.path.join('./uploads', old_filename)
-    processed_file = os.path.join('./uploads', new_filename)
+    uploaded_file = os.path.join('/home/giancarlo_corredor/bucket', old_filename)
+    processed_file = os.path.join('/home/giancarlo_corredor/bucket', new_filename)
     cmd = ['ffmpeg', '-i', uploaded_file,  processed_file]
     task = session.query(Tarea).filter_by(id=taskId).first()
-    # task = Tarea.query.filter(Tarea.id == taskId).first()
     try:
         subprocess.run(cmd, check=True)
         print("Procesando tarea")
@@ -34,3 +42,24 @@ def process_file(old_filename, new_filename, taskId):
         task.estado = "failed"
         session.commit()
         return str(e)
+
+
+project = "misw4204-202315-grupo21"
+subscription = "conversor-sub"
+
+subs = pubsub_v1.SubscriberClient()
+subpath = subs.subscription_path(project, subscription)
+
+while True:
+    res = subs.pull(subscription=subpath, max_messages=1, return_immediately=True)
+
+    if res.received_messages:
+        message = res.received_messages[0].message.data
+        subs.acknowledge(subscription=subpath, ack_ids=[res.received_messages[0].ack_id])
+        # print(message)
+        data = json.loads(message.decode('utf-8'))
+        process_file(data['original'], data['nuevo'], data['id_tarea'])
+    else:
+        print("Nada nuevo")
+
+    time.sleep(10)
