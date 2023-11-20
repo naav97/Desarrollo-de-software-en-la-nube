3afde63e9a79297ec44f41cebe814593331d3fa7
Giancarlo <corredorcatino@users.noreply.github.com>
2023-11-19 23:47:08 -0500

    Worker con pub sub (#24)
    * intento de leer subs
    
    * fix
    
    * ack
    
    * ya funciona
    
    * ultimos cambios
    
    ---------
    
    Co-authored-by: Nicolas Alvarado <na.alvarado@uniandes.edu.co>

--- worker.py
+++ worker.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
-from celery import Celery
+import time
+import json
 from modelos import Tarea
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from google.cloud.sql.connector import Connector, IPTypes
+from google.cloud import pubsub_v1
 
 # initialize Python Connector object
 connector = Connector()
@@ -12,7 +14,7 @@ connector = Connector()
 # Python Connector database connection function
 def getconn():
     conn = connector.connect(
-        "checkurl-396803:us-central1:test01", # Cloud SQL Instance Connection Name
+        "project:region:instance", # Cloud SQL Instance Connection Name
         "pg8000",
         user="user",
         password="password",
@@ -21,24 +23,17 @@ def getconn():
     )
     return conn
 
-app = Celery(broker= "redis://redis:6379/0",backend= "redis://redis:6379/0")
-
-db_uri = 'postgresql://user:password@34.27.104.33:5432/conversor'
-engine = create_engine(db_uri)
+engine = create_engine('postgresql+pg8000://', creator=getconn)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@app.task(name="process_file")
 def process_file(old_filename, new_filename, taskId):
-    uploaded_file = os.path.join('/home/naav972/videos', old_filename)
-    processed_file = os.path.join('/home/naav972/videos', new_filename)
+    uploaded_file = os.path.join('/home/giancarlo_corredor/bucket', old_filename)
+    processed_file = os.path.join('/home/giancarlo_corredor/bucket', new_filename)
     cmd = ['ffmpeg', '-i', uploaded_file,  processed_file]
-    cmd2 = ['gsutil', 'cp', processed_file, 'gs://test-dev-naav97']
     task = session.query(Tarea).filter_by(id=taskId).first()
-    # task = Tarea.query.filter(Tarea.id == taskId).first()
     try:
         subprocess.run(cmd, check=True)
-        subprocess.run(cmd2, check=True)
         print("Procesando tarea")
         task.estado = "processed"
         session.commit()
@@ -48,3 +43,23 @@ def process_file(old_filename, new_filename, taskId):
         session.commit()
         return str(e)
 
+
+project = "misw4204-202315-grupo21"
+subscription = "conversor-sub"
+
+subs = pubsub_v1.SubscriberClient()
+subpath = subs.subscription_path(project, subscription)
+
+while True:
+    res = subs.pull(subscription=subpath, max_messages=1, return_immediately=True)
+
+    if res.received_messages:
+        message = res.received_messages[0].message.data
+        subs.acknowledge(subscription=subpath, ack_ids=[res.received_messages[0].ack_id])
+        # print(message)
+        data = json.loads(message.decode('utf-8'))
+        process_file(data['original'], data['nuevo'], data['id_tarea'])
+    else:
+        print("Nada nuevo")
+
+    time.sleep(10)
